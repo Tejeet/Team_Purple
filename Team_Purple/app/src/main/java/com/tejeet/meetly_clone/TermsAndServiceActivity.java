5a5c6b0a34dd120e29f44f8cc7ad79dbf105a374
Shivhari Banait <shivharibanait98@gmail.com>
2020-12-31 12:56:25 +0530

    Added Features - Privacy Policy, Terms and Condition, Forgot Password

---
+++ Team_Purple/app/src/main/java/com/tejeet/meetly_clone/TermsAndServiceActivity.java
@@ -0,0 +1,14 @@
+package com.tejeet.meetly_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsAndServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_service);
+    }
+}
