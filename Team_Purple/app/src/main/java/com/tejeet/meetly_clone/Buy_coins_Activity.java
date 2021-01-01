c608f2e11208a394c03529d378fe248e8e608e8b
Shivhari Banait <shivharibanait98@gmail.com>
2021-01-01 13:49:06 +0530

    commit Buy Coins Feature

---
+++ Team_Purple/app/src/main/java/com/tejeet/meetly_clone/Buy_coins_Activity.java
@@ -0,0 +1,14 @@
+package com.tejeet.meetly_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buy_coins_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_coins_);
+    }
+}
