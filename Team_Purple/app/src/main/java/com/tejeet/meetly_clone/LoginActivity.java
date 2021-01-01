2766860d0bfc3e5ee14fb5c36f9b0f6c8e341e51
Tejeet <Tejeetm@gmail.com>
2021-01-01 23:58:34 +0530

    Added Features : Stable All Features

--- Team_Purple/app/src/main/java/com/tejeet/meetly_clone/LoginActivity.java
+++ Team_Purple/app/src/main/java/com/tejeet/meetly_clone/LoginActivity.java
@@ -6,14 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.tejeet.meetly_clone.Data.Constants;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TextView mRegister;
+    private TextView mRegister, mForgotPass;
     private Button mFacebook, mLogin;
+    private EditText mEmail, mPass;
 
     private Constants cn;
 
@@ -25,6 +27,11 @@ public class LoginActivity extends AppCompatActivity {
         mRegister = findViewById(R.id.tvRegister);
         mFacebook = findViewById(R.id.btnFacebooklogin);
         mLogin = findViewById(R.id.btnLoginbutton);
+        mForgotPass = findViewById(R.id.tvforgetpassword);
+
+        mEmail = findViewById(R.id.etEmail);
+        mPass = findViewById(R.id.etPassword);
+
 
         cn = new Constants();
 
@@ -44,11 +51,21 @@ public class LoginActivity extends AppCompatActivity {
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                gotoLogin();
-                cn.setLoginStatus(LoginActivity.this, "1");
+
+                if (checkcredential()){
+                    gotoLogin();
+                    cn.setLoginStatus(LoginActivity.this, "1");
+                }
+
             }
         });
 
+        mForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoforgotpass();
+            }
+        });
 
 
 
@@ -59,14 +76,37 @@ public class LoginActivity extends AppCompatActivity {
         finish();
     }
 
-    public void gotofacebooklogin() {
-        startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+    public void gotoforgotpass() {
+        startActivity(new Intent(LoginActivity.this, ForgotpassActivity.class));
         finish();
     }
 
     public void gotosignup() {
         startActivity(new Intent(LoginActivity.this, SignupActivity.class));
         finish();
+    }
+
+
+    private boolean checkcredential(){
+
+        boolean isValid = true;
+
+        if (mEmail.getText().toString().isEmpty()){
+            mEmail.setError("Please enter Email ID");
+            isValid = false;
+        }
+
+        if (mPass.getText().toString().isEmpty()){
+            mPass.setError("Please enter Email ID");
+            isValid = false;
+        }
+
+        if (mEmail.getText().toString().contains("@") && mPass.getText().toString().length() >= 6){
+            isValid = true;
+        }
+
+        return isValid;
+
     }
 
 
